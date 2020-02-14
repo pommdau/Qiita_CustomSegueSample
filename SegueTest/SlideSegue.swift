e312bf97c9d38c67b80a0f5afd1fe10a628efd4b
hikeuchi <hikeuchi@morisawa.co.jp>
2020-02-14 13:43:52 +0900

    ウィンドウ変形の際のアニメーションを実装

--- SegueTest/SlideSegue.swift
+++ SegueTest/SlideSegue.swift
@@ -21,19 +21,17 @@ class SlideSegue: NSStoryboardSegue {
             parentViewController.addChild(destinationViewController)
         }
         
-        if let window = sourceViewController.view.window  {
-            let contentsViewHeightOffset = sourceViewController.view.frame.height - destinationViewController.view.frame.height
-            let titleBarHeightOffset = window.frame.height - sourceViewController.view.frame.height // Titlebar.height分
-            let newRect = NSRect(x: window.frame.origin.x,
-                                 y: window.frame.origin.y + contentsViewHeightOffset,
-                                 width: destinationViewController.view.frame.width,
-                                 height: destinationViewController.view.frame.height + titleBarHeightOffset
-)
-            window.setFrame(newRect, display: true)
-        }
+        let window = sourceViewController.view.window!
+        let contentsViewHeightOffset = sourceViewController.view.frame.height - destinationViewController.view.frame.height
+        let titlebarHeight = window.frame.height - sourceViewController.view.frame.height // タイトルバーの高さ
+        let newFrame = NSRect(x: window.frame.origin.x,
+                              y: window.frame.origin.y + contentsViewHeightOffset,
+                              width: destinationViewController.view.frame.width,
+                              height: destinationViewController.view.frame.height + titlebarHeight
+        )
         
         sourceViewController.view.superview?.addSubview(destinationViewController.view) // ContainerViewに追加
-        sourceViewController.view.removeFromSuperview()
+        sourceViewController.view.removeFromSuperview() // 遷移前のビューを削除
         
         // Constraintsの設定
         destinationViewController.view.translatesAutoresizingMaskIntoConstraints = false
@@ -41,5 +39,13 @@ class SlideSegue: NSStoryboardSegue {
         destinationViewController.view.trailingAnchor.constraint(equalTo: parentViewController.view.trailingAnchor).isActive = true
         destinationViewController.view.topAnchor.constraint(equalTo: parentViewController.view.topAnchor).isActive = true
         destinationViewController.view.bottomAnchor.constraint(equalTo: parentViewController.view.bottomAnchor).isActive = true
+        
+        // アニメーション的にウィンドウを変形する
+        destinationViewController.view.isHidden = true   // ウィンドウサイズが変更された後に内容を表示するため
+        NSAnimationContext.runAnimationGroup({ _ in
+            window.animator().setFrame(newFrame, display: false)
+        }, completionHandler: { [weak self] in
+            destinationViewController.view.isHidden = false
+        })
     }
 }
